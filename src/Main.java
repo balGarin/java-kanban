d8bd1102e308ffbb2e6ea6f1abd6672d20f8d378
gabaNet66 <rin9a@yandex.ru>
2024-06-25 01:56:09 +0300

    feat : Откат

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,12 @@
-import managers.*;
-import tasks.Epic;
-import tasks.Status;
-import tasks.Subtask;
-import tasks.Task;
 
-import java.util.List;
+import managers.*;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        TaskManager manager=Managers.getDefault();
-
-
-
-
-
-
-
-
-
+        TaskManager manager = Managers.getDefault();
     }
+
+
 }
